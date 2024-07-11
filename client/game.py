63bd96b3ad970156a2fc0904cec0546a47411086
Andrew Helbig <helbigventures@gmail.com>
2024-07-11 11:53:31 -0700

    switching computers

--- client/game.py
+++ client/game.py
@@ -10,6 +10,15 @@ class Game:
         self.player_hands = list()
         self.dealer_hands = list()
 
+        self.window_size
+
+        self.main_menu = pygame_menu.Menu('Break The Bank',
+                                        400, 
+                                        300,
+                                        theme=pygame_menu.themes.THEME_DARK)
+        self.join_menu = 
+
+
 
     def join_game(self):
         print("join")
@@ -24,13 +33,23 @@ class Game:
         pygame.init()
         surface = pygame.display.set_mode((600, 400))
 
-        menu = pygame_menu.Menu('Blackjack', 400, 300,
+        main_menu = pygame_menu.Menu('Break The Bank', 400, 300,
                             theme=pygame_menu.themes.THEME_DARK)
+        
+        main_menu.add.text_input('Player Name: ', default='Ass Hoe')
+        main_menu.add.button('Start Game', self.start_game)
+        main_menu.add.button('Join Game', self.join_game)
+        main_menu.add.button('Quit', pygame_menu.events.EXIT)
+        
+        while True:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    exit()
 
-        menu.add.text_input('Player Name: ', default='Ass Hoe')
-        menu.add.button('Start Game', self.start_game)
-        menu.add.button('Join Game', self.join_game)
-        menu.add.button('Quit', pygame_menu.events.EXIT)
+            if main_menu.is_enabled():
+                main_menu.mainloop(surface)
 
-        menu.mainloop(surface)
+            pygame.display.flip()
+
         pygame.quit()
